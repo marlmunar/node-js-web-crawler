514a1281bd8df19e107bf7f3f36b7bab24538a67
marlmunar <marlemmmunar@gmail.com>
2025-06-25 15:37:05 +0800

    Checked the normalizer fn for text case consistency and http protocol

--- crawl.test.js
+++ crawl.test.js
@@ -14,3 +14,17 @@ test("normalizeURL strip trailing slashes", () => {
   const expected = "blog.boot.dev/path";
   expect(actual).toEqual(expected);
 });
+
+test("normalizeURL check case", () => {
+  const input = "https://BLOG.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip http", () => {
+  const input = "http://BLOG.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
